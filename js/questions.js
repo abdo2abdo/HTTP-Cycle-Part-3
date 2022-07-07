7abf3aa576abd786559f71172cd2723c29cac7c7
AbdulRahman Awad <bw3169034@gmail.com>
2022-07-07 17:10:06 +0200

    http cycle part 3

---
+++ js/questions.js
@@ -0,0 +1,94 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The response consists of two things?",
+    answer: "Content & Status Code",
+    options: [
+      "Content & js",
+      "Content & xml",
+      "Content & Status Code",
+      "Content & json"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Examples of content",
+    answer: "ALL",
+    options: [
+      "HTML & CSS & JS",
+      "XML",
+      "JSON",
+      "ALL"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Status Code divided into numer of category?",
+    answer: "5",
+    options: [
+      "4",
+      "3",
+      "7",
+      "5"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Success Request",
+    answer: "200 Ok",
+    options: [
+      "201 Created",
+      "202 Accepted",
+      "200 Ok",
+      "204 No Content"
+    ]
+  },
+    {
+    numb: 5,
+    question: "person unregister login?",
+    answer: "401 Unauthorized",
+    options: [
+      "400 Bad Request",
+      "401 Unauthorized",
+      "403 Forbidden",
+      "404 Not Found"
+    ]
+  },
+  {
+    numb: 6,
+    question: "The request URL not found?",
+    answer: "404 Not Found",
+    options: [
+      "400 Bad Request",
+      "401 Unauthorized",
+      "403 Forbidden",
+      "404 Not Found"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Syntax error in code?",
+    answer: "500 Internal Server Error",
+    options: [
+      "500 Internal Server Error",
+      "501 Not Implemented",
+      "502 Bad Gateway",
+      "503 Service Unavailable"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
